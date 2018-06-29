7bad53c8c8db13b26228628cffed8fd4d5e401fd
chamoddamitha <chamod.14@cse.mrt.ac.lk>
2018-06-29 23:09:28 +0530

    Add aspect analyzer ui

--- aspects.js
+++ aspects.js
@@ -4,11 +4,37 @@
 var express = require('express');
 var router = express.Router();
 
-//
-// router.get('/', function (req, res) {
-//
-// });
+const { check, validationResult } = require('express-validator/check')
 
+router.get('/', function (req, res) {
+    res.render('aspects');
+});
+
+router.post('/findAspects', [
+    check('message')
+        .isLength({min: 1})
+        .withMessage('Review is required'),
+], (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.render('aspects', {
+            data: req.body,
+            errors: errors.mapped()
+        })
+    }
+
+    //find aspects and sentiments
+    aspects  = [
+        {aspect : 'food', sentiment : -1},
+        {aspect : 'drinks', sentiment : 0},
+        {aspect : 'service', sentiment : 1},
+    ]
+
+    return res.render('aspects', {
+        data: req.body,
+        aspects: aspects,
+    })
+})
 
 //export this router to use in our index.js
 module.exports = router;
--- index.js
+++ index.js
@@ -4,23 +4,44 @@
 var express = require('express');
 var app = express();
 var tweets = require('./tweets');
+var aspects = require('./aspects');
 
 const exphbs = require('express-handlebars');
-const path = require('path');
 
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/node_modules'));
+const path = require('path');
+const bodyParser = require('body-parser')
+const validator = require('express-validator')
 
 app.engine('.hbs', exphbs({
+    helpers: {
+        equal: function (lvalue, rvalue, options) {
+            if (arguments.length < 3)
+                throw new Error("Handlebars Helper equal needs 2 parameters")
+            if (lvalue != rvalue) {
+                return options.inverse(this)
+            } else {
+                return options.fn(this)
+            }
+        },
+    },
     defaultLayout: 'main',
     extname: '.hbs',
     layoutsDir: path.join(__dirname, 'views/layouts')
 }));
 
+const middleware = [
+    express.static(path.join(__dirname, '/public')),
+    express.static(__dirname + '/node_modules'),
+    bodyParser.urlencoded(),
+    validator(),
+]
+app.use(middleware)
+
 app.set('view engine', '.hbs');
 app.set('views', './views');
 
 app.use('/tweets', tweets);
+app.use('/aspects', aspects);
 
 //home route
 app.get('/', function (req, res) {
